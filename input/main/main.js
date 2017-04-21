8b1a30f5d3f2deeae5ab0570deb23944a9ec837f
Masis Gulmez <masis@iMac-de-Masis.local>
2017-04-21 15:36:44 +0200

    input

--- input/main/main.js
+++ input/main/main.js
@@ -1,26 +1,38 @@
-var input = document.querySelector('input');
-var label = document.querySelector('label');
+var input = document.querySelectorAll('input');
+var label = document.querySelectorAll('label');
 var eye = document.querySelector('i.fa-eye');
 var eye_closed = document.querySelector('i.fa-eye-slash');
 
-input.addEventListener('focus', function(){
-	label.style.top = '-30px';
-});
-input.addEventListener('blur', function(){
-	label.style.top = '0px';
-	if (input.value) {
-		label.style.top = '-30px';
-	}
-});
+
+for (var i = 0; i < input.length; i++) {
+	input[i].addEventListener('focus', function(){
+		var parent = this.parentElement;
+		parent.children[1].style.top = '-30px';
+	});
+	input[i].addEventListener('blur', function(){
+		var parent = this.parentElement;
+		var child = parent.children[0];
+
+		if (child.value) {
+			parent.children[1].style.top = '-30px';
+		} else {
+			parent.children[1].style.top = '0px';
+		}
+	});
+}
 
 eye.addEventListener('click', function(){
-	input.setAttribute('type', 'text');
+	var parent = this.parentElement;
+	var child = parent.children[0];
+	child.setAttribute('type', 'text');
 	this.classList.add('closed');
 	eye_closed.classList.remove('closed');
 });
 
 eye_closed.addEventListener('click', function(){
-	input.setAttribute('type', 'password');
+	var parent = this.parentElement;
+	var child = parent.children[0];
+	child.setAttribute('type', 'password');
 	this.classList.add('closed');
 	eye.classList.remove('closed');
 });
